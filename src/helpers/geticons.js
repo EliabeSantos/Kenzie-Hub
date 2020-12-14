276926f869751a2cd4a80d23811258dd6ea96b2d
BrunoGehlen <tousepc@gmail.com>
2020-12-14 14:39:20 -0300

    Feat: add default icon for icon dictionary

--- src/helpers/geticons.js
+++ src/helpers/geticons.js
@@ -8,16 +8,18 @@ import Ruby from "../helpers/icons/icons8-ruby-programming-language.svg";
 import Typescript from "../helpers/icons/icons8-typescript.svg";
 import Vue from "../helpers/icons/icons8-vue-js.svg";
 import React from "../helpers/icons/icons8-react.svg";
+import DefaultIcon from '../images/logo.svg'
 
 export const dictionaryIcons = {
     bootstrap: Bootstrap,
-    react: React,        
-    jcons8c: Icons8c,        
-    java: Java,        
-    jquery: Jquery,        
-    material: Material,        
-    python: Python,        
-    ruby: Ruby,        
-    typescript: Typescript,        
+    react: React,
+    jcons8c: Icons8c,
+    java: Java,
+    jquery: Jquery,
+    material: Material,
+    python: Python,
+    ruby: Ruby,
+    typescript: Typescript,
     vue: Vue,
+    default: DefaultIcon
 }
